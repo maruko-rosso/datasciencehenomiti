2bcb1cddc00291e666e272e110a7cc3b6083c44c
maruko-rosso <59382087+maruko-rosso@users.noreply.github.com>
2020-01-03 11:44:11 +0900

    Update post-463.r

--- post-463.r
+++ post-463.r
@@ -1,3 +1,5 @@
+
+#### 文字列の切り出し ####
 > # テストデータ 
 > TestData <- c("AABBCCDD", "ABCDABCDABCD", "AABBCCDD") 
 > 
@@ -8,3 +10,27 @@
 > # 文字列の切り出し２ 
 > substring(TestData, 2, 3) 
 [1] "AB" "BC" "AB"
+
+#### 文字列の切り出し ####
+> # テストデータ
+> TestData <- c("AABBCCDD", "ABCDABCDABCD", "AABBCCDD")
+> 
+> # sub
+> sub("A", "", TestData)
+[1] "ABBCCDD"     "BCDABCDABCD" "ABBCCDD"    
+> 
+> # gsub
+> gsub("A", "", TestData)
+[1] "BBCCDD"    "BCDBCDBCD" "BBCCDD"
+
+#### 文字列の結合 ####
+> # テストデータ 
+> TestData <- c("AABBCCDD", "ABCDABCDABCD", "AABBCCDD") 
+> 
+> # 文字列の１つ目と２つ目を結合 
+> paste(TestData[1], TestData[2]) 
+[1] "AABBCCDD ABCDABCDABCD" 
+> 
+> # 文字列の１つ目と２つ目を結合し間の空白を無くす 
+> paste(TestData[1], TestData[2], sep = "") 
+[1] "AABBCCDDABCDABCDABCD"
