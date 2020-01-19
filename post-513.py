04be24cfe30b4806d3859a7b58f0ac921fa0d583
maruko-rosso <59382087+maruko-rosso@users.noreply.github.com>
2020-01-19 15:22:30 +0900

    Update post-513.py

--- post-513.py
+++ post-513.py
@@ -9,3 +9,26 @@ from matplotlib import pyplot as plt
 
 # jupyter notebookへのグラフの埋め込み 
 %matplotlib inline
+
+ 
+
+# seabornを利用したグラフの可視化
+import seaborn as sns
+sns.set() # おまじない
+
+# グラフの可視化
+plt.plot(x, y)
+plt.title("lineplot matplotlib")
+plt.xlabel("x")
+plt.ylabel("y")
+
+
+df = pd.DataFrame({
+    "x" : x,
+    "y" : y})
+df
+sns.pointplot(x = "x", y = "y", data = df)
+plt.title("lineplot by seaborn")
+ 
+
+
